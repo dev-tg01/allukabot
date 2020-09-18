e19a44814333a45c7262b731d35191578abf41d9
Divu <70699947+imdivu@users.noreply.github.com>
2020-09-18 14:17:49 +0530

    Update feedback.py

--- alluka/modules/feedback.py
+++ alluka/modules/feedback.py
@@ -13,8 +13,7 @@ def feedback(bot: Bot, update: Update):
   name = update.effective_message.from_user.first_name
   message = update.effective_message
   userid=message.from_user.id
-  text = message.text[len('/feedback'):]
-  text2 = message.text[len('/feedback@MissHinata_Bot'):] 
+  text = message.text[len('/feedback')('/feedback@MissHinata_Bot'):]
 
   feed_text = f"Hinata's *New* Feedback From [{name}](tg://user?id={userid})\n\nFeed→一>→ : {text} {text2}"
   
