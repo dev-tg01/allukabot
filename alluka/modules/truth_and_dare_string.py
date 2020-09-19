3f7a77f8f5ce347c71086da6afbd6615e289b944
Divu <70699947+imdivu@users.noreply.github.com>
2020-09-19 12:28:11 +0530

    Update truth_and_dare_string.py

--- alluka/modules/truth_and_dare_string.py
+++ alluka/modules/truth_and_dare_string.py
@@ -42,7 +42,7 @@ TRUTH = (
     "If you had to trade your friend in for the celebrity crush of your dreams, which friend would you choose?",
     "Would you share a toothbrush with your best friend?",
     "Who's hotter? You or your friend?",
-    "Who Is Stupid? (A. Your Friend  (B. You",
+    "Who Is Stupid?\n (A. Your Friend\n  (B. You",
     "Have you ever told a lie about your best friend to make yourself look better?",
     "What's one food that you will never order at a restaurant?",
     "What's one thing you would never eat on a first date?",
